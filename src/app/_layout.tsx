214339fbfb958a3c8092bfc1808ade28d0154794
Dailton Bastos <dailtonbastos@gmail.com>
2024-10-05 17:07:30 -0300

    fix: useFonts is not loading fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,23 +8,23 @@ import 'react-native-reanimated'
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-	const [loaded] = useFonts({
+	const [loaded, error] = useFonts({
 		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
 	})
 
 	useEffect(() => {
-		if (loaded) {
+		if (loaded || error) {
 			SplashScreen.hideAsync()
 		}
-	}, [loaded])
+	}, [loaded, error])
 
-	if (!loaded) {
+	if (!loaded && !error) {
 		return null
 	}
 
 	return (
 		<Stack>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen name="index" options={{ headerShown: false }} />
 			<Stack.Screen name="+not-found" />
 		</Stack>
 	)
